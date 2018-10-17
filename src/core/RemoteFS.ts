3bf5358f9c1cb2598941cdcb5d2ddb34843aa7e8
dingguo <dingguo@wacai.com>
2018-10-18 03:20:16 +0800

    feat: remove unused code.

--- src/core/RemoteFS.ts
+++ src/core/RemoteFS.ts
@@ -74,21 +74,6 @@ function _connect(config: ConnectConfig) {
 }
 
 /**
- * 顺序执行 Promise
- * @param tasks 任务们
- */
-function sequenceTasks(tasks) {
-  function recordValue(results, value) {
-    results.push(value);
-    return results;
-  }
-  const pushValue = recordValue.bind(null, []);
-  return tasks.reduce((promise, task) => {
-    return promise.then(task).then(pushValue);
-  }, Promise.resolve());
-}
-
-/**
  * 1. 连接服务器
  * 2. 创建所有文件夹
  * 3. 上传所有文件
@@ -130,36 +115,6 @@ function _performUpload(config: ConnectConfig) {
       // 3. 上传所有文件
       const allPromise = [];
       for (const temp of fileMap) {
-        // const tempPromise = () =>
-        //   conn.sftp('exists', { path: temp.remote }).then(fileExist => {
-        //     if (fileExist) {
-        //       // return conn.sftp('delete', { path: temp.remote }).then(() => {
-        //       //   return conn.sftp('createWriteStream', {
-        //       //     path: temp.remote,
-        //       //     data: Fs.createReadStream(temp.local),
-        //       //   });
-        //       // });
-        //       const deleteAction = () => {
-        //         return conn.sftp('delete', { path: temp.remote });
-        //       };
-        //       const uploadAction = () => {
-        //         return conn.sftp('createWriteStream', {
-        //           path: temp.remote,
-        //           data: Fs.createReadStream(temp.local),
-        //         });
-        //       };
-        //       return sequenceTasks([deleteAction, uploadAction]);
-        //     } else {
-        //       const uploadAction = () => {
-        //         conn.sftp('createWriteStream', {
-        //           path: temp.remote,
-        //           data: Fs.createReadStream(temp.local),
-        //         });
-        //       };
-        //       return sequenceTasks([uploadAction]);
-        //     }
-        //   });
-
         const tempPromise = conn.sftp('createWriteStream', {
           path: temp.remote,
           data: Fs.createReadStream(temp.local),
@@ -167,7 +122,6 @@ function _performUpload(config: ConnectConfig) {
         allPromise.push(tempPromise);
       }
 
-      // return sequenceTasks(allPromise);
       return Promise.all(allPromise);
     })
     .then(() => {
@@ -181,65 +135,6 @@ function _performUpload(config: ConnectConfig) {
       conn.end();
       spinner.stop();
     });
-}
-
-function _testLoading() {
-  const spinner = ora('Loading config').start();
-  spinner.color = 'yellow';
-
-  console.log('directory list: ' + remoteDirectory);
-  console.log();
-  setTimeout(() => {
-    spinner.succeed('Loading success, enjoy it!');
-  }, 3000);
-  for (const temp of fileMap) {
-    console.log('local: ' + temp.local);
-    console.log('remote: ' + temp.remote);
-  }
-  setTimeout(() => {
-    spinner.stop();
-  }, 5000);
-}
-
-function _testRx() {
-  const observable = Rx.Observable.create(observer => {
-    observer.next('hi');
-    observer.next('world');
-    setTimeout(() => {
-      observer.next('这是一段一步操作');
-    }, 30);
-    observer.complete();
-  }).pipe(map((value: string) => value.repeat(2)));
-  console.log('start');
-  const subscription = observable.subscribe(value => {
-    console.log(value);
-  });
-  console.log('end');
-  setTimeout(() => {
-    subscription.unsubscribe();
-  }, 5000);
-}
-
-function _readdirAsync(path: string): Promise<string[]> {
-  return new Promise<string[]>((resolve, reject) => {
-    Fs.readdir(path, (err, files) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(files);
-    });
-  });
-}
-
-function _statAsync(path: string): Promise<Fs.Stats> {
-  return new Promise<Fs.Stats>((resolve, reject) => {
-    Fs.stat(path, (err, stats) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(stats);
-    });
-  });
 }
 
 function _readdirSync(local: string, remote: string, depth: number) {
@@ -262,55 +157,4 @@ function _readdirSync(local: string, remote: string, depth: number) {
       });
     }
   });
-}
-
-async function _recursiveReaddir(local: string, remote: string, depth: number) {
-  // 根据文件路径读取文件，返回文件列表
-  Fs.readdir(local, (err, files) => {
-    if (err) {
-      console.warn(err);
-    } else {
-      // 遍历读取到的文件
-      files.forEach(filename => {
-        // 获取当前文件的绝对路径
-        const localDir = Path.join(local, filename);
-        // 获取远程当前文件的绝对路径
-        const remoteDir = Path.join(remote, filename);
-        // 根据文件路径获取文件信息
-        Fs.stat(localDir, (error, stats) => {
-          if (error) {
-            console.warn('stats failed');
-          } else {
-            if (stats.isFile()) {
-              console.log(_getTab(depth), localDir);
-              fileMap.push({
-                local: localDir,
-                remote: remoteDir,
-              });
-
-              // const readable = Fs.createReadStream(localDir);
-              // const writable = Fs.createWriteStream(remoteDir);
-              // readable.pipe(writable);
-            }
-            if (stats.isDirectory()) {
-              // Fs.exists(remoteDir, exists => {
-              //   if (exists) {
-              //     _recursiveReaddir(localDir, remoteDir, depth + 1);
-              //   } else {
-              //     Fs.mkdir(remoteDir, () => {
-              //       _recursiveReaddir(localDir, remoteDir, depth + 1); // 开始递归
-              //     });
-              //   }
-              // });
-              _recursiveReaddir(localDir, remoteDir, depth + 1);
-            }
-          }
-        });
-      });
-    }
-  });
-}
-
-function _getTab(count: number) {
-  return '    '.repeat(count);
 }
